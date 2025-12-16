035f138d099de777246a16f9cb48d00cdf7b31ee
Wale <134291319+gh9727@users.noreply.github.com>
2025-12-16 21:25:38 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 7.87 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
 
 ### 성능 요약
 
-메모리: 177 MB, 시간: 198.91 ms
+메모리: 90.8 MB, 시간: 7.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 00:20:33
+2025년 12월 16일 21:25:36
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,52 +1,16 @@
-import java.util.ArrayList;
-// (p > t) 인 t의 개수
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        int size = p.length();
+        int len = t.length() - p.length() + 1;
+        int pLen = p.length();
+        Long pNum = Long.parseLong(p);
         
-        // 1. p의 부분 문자열 == t의 부분 문자열
-        if(t.length() == p.length()) {
-            long n1 = Long.parseLong(t);
-            long n2 = Long.parseLong(p);
-            return n1 > n2 ? 0 : 1;
-        }
-        
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < t.length(); i++) {
-            list.add(String.valueOf(t.charAt(i)));
-        }
-        
-        while(true) {
-            if(list.size() < p.length()) {
-                break;
+        for(int i = 0; i < len; i++) {
+            Long tNum = Long.parseLong(t.substring(i, i + pLen));
+            if(tNum <= pNum) {
+                answer += 1;
             }
-            t = "";
-            for(int i = 0; i < size; i++) {
-                t += list.get(i);
-            }   
-            System.out.println("size = " + size);
-            System.out.printf("t = %s",t);
-            // 마지막즈음 부분문자열 개수가 맞지 않을 수 있음
-            if(t.length() == p.length()) {
-                long n1 = Long.parseLong(t);
-                long n2 = Long.parseLong(p);
-                answer = n1 > n2 ? (answer) : (answer + 1);    
-            }
-            else {
-                break;
-            }
-            list.remove(0);
         }
-        
         return answer;
     }
 }
-
-
-
-
-
-// b. t.length > p.length
-// Integer.parseInt(t);
-// Integer.parseInt(p);
