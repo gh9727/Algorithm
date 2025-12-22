9936d4619290300198210fb1a1b59beec10e4697
Wale <134291319+gh9727@users.noreply.github.com>
2025-12-22 22:08:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 0.02 ms
+메모리: 70.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 29일 22:44:59
+2025년 12월 22일 22:08:27
 
 ### 문제 설명
 
--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,13 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int su = 0;
+        int answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        
         for(int i = 0; i < numbers.length; i++) {
-            su += numbers[i];
+            answer -= numbers[i];
         }
-        answer = 45 - su;
+        
         return answer;
     }
 }
-
-// 
