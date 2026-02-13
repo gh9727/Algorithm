e339bdc72161d01a1e66130dbfb08dbc51a23515
Wale <134291319+gh9727@users.noreply.github.com>
2026-02-13 10:20:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.10 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        
+        if(s.length() == 4 || s.length() == 6) {
+            try {
+                int num = Integer.parseInt(s);
+                return true;
+            } 
+            catch(NumberFormatException e) {
+                return false;
+            }   
+        }
+        return false;
+    }
+}
