0c3edc601e8212044cc047077239ce3d00fef99b
Wale <134291319+gh9727@users.noreply.github.com>
2026-03-18 11:01:28 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+    String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    int total = b - 1; 
+    
+    for (int i = 0; i < a - 1; i++) {
+        total += month[i];
+    }
+    return day[total % 7];
+    
+    }
+}
